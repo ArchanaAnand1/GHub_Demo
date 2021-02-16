398b7efdb97376a382ac385af39f92d5108560ac
Archana Anand <archana.anand53@gmail.com>
2021-02-16 22:41:18 +0530

    add all

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "671f368f-49c7-4aa9-8647-8d72fd149877",
+    "id": "bb6fb211-7808-4015-9a2c-8c53151f4c3d",
     "feature": "Free CRM login",
     "scenario": "Free CRM login test cenarios",
-    "start": 1612637926586,
+    "start": 1613495251728,
     "group": 15,
     "content": "",
     "tags": "@smoke,@regression,",
-    "end": 1612637942903,
+    "end": 1613495308805,
     "className": "passed"
   },
   {
-    "id": "57f92829-d8a2-4f37-a43a-a5db5b40b075",
+    "id": "f33e4ca4-aa33-4503-ac1f-4467c854a7f0",
     "feature": "Free CRM login",
     "scenario": "Free CRM login test cenarios",
-    "start": 1612637926586,
+    "start": 1613495251912,
     "group": 14,
     "content": "",
     "tags": "@sanity,",
-    "end": 1612637958876,
+    "end": 1613495333109,
     "className": "passed"
   }
 ]);
